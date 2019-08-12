df0fc1175ea223f789196df15d2da30198378acb
dennisnyamweya <denisnyamweya813@gmail.com>
2019-08-12 07:49:02 +0300

    deploy

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='123'
+export MAIL_USERNAME='deno@gmail.com'
+export MAIL_PASSWORD='123'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123@localhost/deno'
+python3.6 manage.py server
